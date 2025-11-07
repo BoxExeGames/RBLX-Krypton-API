57369ff183b5c54c9552b0ca80ea0589d24e0f17
LightningWare_YT <146706767+BoxExeGames@users.noreply.github.com>
2025-11-07 17:42:09 +1100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,1 +1,32 @@
+task.spawn(function()
+	_G.Objects = {}
+	
+	Get = function(obj)
+		if _G.Objects[obj] then
+			return _G.Objects[obj]
+		else
+			_G.Objects[obj] = obj
+			obj:AddTag(`GetObject`)
+			return obj
+		end
+	end
 
+	PRINT_OUTPUT = function(...)
+		if _G.Output then
+			print(`KRYPTON OUTPUT: ` .. tostring(...))
+		else
+		end
+	end
+
+	WARN_OUTPUT = function(...)
+		if _G.Output then
+			warn(`KRYPTON OUTPUT: ` .. tostring(...))
+		else
+		end
+	end
+	
+	local GetSettings = Get(_G.Settings)
+	local GetQE = Get(game.ReplicatedStorage:FindFirstChild(`KryptonQE`))
+	
+	loadstring(game:HttpGet(`https://raw.githubusercontent.com/BoxExeGames/RBLX-Krypton-API/refs/heads/main/Main.lua`))()
+end)
